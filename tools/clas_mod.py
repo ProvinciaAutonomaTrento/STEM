3997a49b0564ef745821253a01cf375201c9e86a
Luca Delucchi <lucadeluge@gmail.com>
2014-12-11 12:14:27 +0100

    create interface for clas_mod script

--- tools/clas_mod.py
+++ tools/clas_mod.py
@@ -31,6 +31,8 @@ from qgis.core import *
 from qgis.gui import *
 
 from stem_base_dialogs import BaseDialog
+from grass_stem import helpUrl
+
 
 class STEMToolsDialog(BaseDialog):
     def __init__(self, iface, name):
@@ -39,6 +41,41 @@ class STEMToolsDialog(BaseDialog):
         self.iface = iface
 
         self._insertSingleInput()
+        items = ['automatico', 'area', 'manuale']
+        label = "Seleziona il metodo da utilizzare"
+        self._insertFirstCombobox(items, label, 1)
+        self.BaseInputCombo.currentIndexChanged.connect(self.operatorChanged)
+        label2 = "Inserire la dimensione minima da tenere in considerazione"
+        self._insertFirstLineEdit(label2, 2)
+        labelText = "Inserire le regole per la riclassificazione secondo lo " \
+                    "stile richiesto dal comando di GRASS GIS r.reclass." \
+                    "Per maggiori informazioni consultare l'help"
+        self._insertTextArea(labelText, 3)
+        self.LabelTextarea.setEnabled(False)
+        self.TextArea.setEnabled(False)
+        self.LabelLinedit.setEnabled(False)
+        self.Linedit.setEnabled(False)
+        self.helpui.fillfromUrl(helpUrl('r.clump'))
+
+    def operatorChanged(self):
+        if self.BaseInputCombo.currentText() == 'automatico':
+            self.LabelTextarea.setEnabled(False)
+            self.TextArea.setEnabled(False)
+            self.LabelLinedit.setEnabled(False)
+            self.Linedit.setEnabled(True)
+            self.helpui.fillfromUrl(helpUrl('r.clump'))
+        elif self.BaseInputCombo.currentText() == 'manuale':
+            self.LabelTextarea.setEnabled(True)
+            self.TextArea.setEnabled(True)
+            self.LabelLinedit.setEnabled(False)
+            self.Linedit.setEnabled(False)
+            self.helpui.fillfromUrl(helpUrl('r.reclass'))
+        else:
+            self.LabelTextarea.setEnabled(False)
+            self.TextArea.setEnabled(False)
+            self.LabelLinedit.setEnabled(True)
+            self.Linedit.setEnabled(True)
+            self.helpui.fillfromUrl(helpUrl('r.reclass.area'))
 
     def show_(self):
         self.switchClippingMode()
