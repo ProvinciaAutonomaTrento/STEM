62c25fb69eb57ba02d448d29c37d58ea8d69f8b4
Luca Delucchi <lucadeluge@gmail.com>
2015-02-02 11:20:07 +0100

    moved files; add new path to sys.path

--- stem_plugin.py
+++ stem_plugin.py
@@ -35,6 +35,9 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              "tools"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             "libs"))
+
 
 from stem_base_dialogs import SettingsDialog
 from stem_toolbox import STEMToolbox
