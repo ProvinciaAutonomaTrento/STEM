88e0845fc3b705fd76a8a363b118b459df6fcea7
Pietro Zambelli <peter.zamb@gmail.com>
2015-02-10 10:13:36 +0100

    Move argparse_checker function in ml

--- ml/ml.py
+++ ml/ml.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 
 # from python standard library
+import imp
 import gc
 import os
 import time
@@ -677,10 +678,38 @@ if __name__ == "__main__":
     import sys
     import argparse
     from datetime import datetime
-    from pprint import pprint
 
-    from argparse_checker import importable, indexstr, pca_components
+    # =======================================================================
+    # Parser functions
+    def pca_components(string):
+        try:
+            return int(string)
+        except ValueError:
+            return None if string == '' else string
 
+    def importable(path):
+        return imp.load_source("classifiers", path)
+
+    def index(string, sep=',', rangesep='-'):
+        """
+        >>> indx = '1-5,34-36,40'
+        >>> [i for i in get_indexes(indx)]
+        [1, 2, 3, 4, 5, 34, 35, 36, 40]
+        """
+        if string == '':
+            return
+        for ind in string.split(sep):
+            if rangesep in ind:
+                start, stop = ind.split(rangesep)
+                for i in range(int(start), int(stop) + 1):
+                    yield i
+            else:
+                yield int(ind)
+
+    def indexstr(string):
+        return [i for i in index(string)]
+
+    # =======================================================================
     # Define the parser options
     parser = argparse.ArgumentParser(description='Test several machine-'
                                                  'learning models.')
