b5685dd6007cabafba54aa71c481fcf3de0d875c
Luca Delucchi <lucadeluge@gmail.com>
2015-03-31 11:59:21 +0200

    fixed copyright into configuration file

--- docstring/source/conf.py
+++ docstring/source/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'STEM develpoment documentation'
-copyright = u'2015, Luca Delucchi', Salvatore La Rosa, Pietro Zambelli
+copyright = u'2015, Luca Delucchi, Salvatore La Rosa, Pietro Zambelli'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
