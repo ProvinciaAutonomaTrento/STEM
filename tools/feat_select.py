3c2675b8a5a12517c4ee00f37f71b16df5124bd1
Luca Delucchi <lucadeluge@gmail.com>
2015-06-30 15:40:22 +0200

    improved feature selection adding the maximum number of features

--- tools/feat_select.py
+++ tools/feat_select.py
@@ -58,6 +58,8 @@ class STEMToolsDialog(BaseDialog):
         mets = ['mean', 'min', 'median']
         self.lm = "Selezione la strategia da utilizzare"
         self._insertMethod(mets, self.lm, 0)
+        labeln = "Numero massimo di feature da selezionare"
+        self. _insertFirstLineEdit(labeln, 1)
         self.connect(self.BrowseButton, SIGNAL("clicked()"),
                      partial(self.BrowseDir, self.TextOut, '.txt'))
 
@@ -119,7 +121,7 @@ class STEMToolsDialog(BaseDialog):
             # ------------------------------------------------------------
             # Extract training samples
             trnpath = os.path.join(home,
-                                   "{pref}_csvtraining.csv".format(pref=prefcsv))
+                                   "{pr}_csvtraining.csv".format(pr=prefcsv))
             log.debug('    From:')
             log.debug('      - vector: %s' % mltb.vector)
             log.debug('      - training column: %s' % mltb.column)
@@ -135,7 +137,8 @@ class STEMToolsDialog(BaseDialog):
 
             # --------------------------------------------------------------
             # Feature selector
-            fselector = SSF(strategy=getattr(np, meth))
+            fselector = SSF(strategy=getattr(np, meth),
+                            n_features=int(self.Linedit.text()))
 
             # ------------------------------------------------------------
             # Transform the input data
