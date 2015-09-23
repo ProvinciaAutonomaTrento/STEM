b30fba09ad930475e2ad53566e4d23c92be6c8a3
Luca Delucchi <lucadeluge@gmail.com>
2015-09-23 11:05:38 +0200

    improve create_package.sh script

--- create_package.sh
+++ create_package.sh
@@ -1,6 +1,7 @@
 DIR=`pwd`
 
 mkdir -p /tmp/stem/
+rm -f tools/*.pyc libs/*.pyc 
 cp -prf images/ libs/ *.py tools/ ui/ uml/ metadata.txt docs/build/latex/DocumentazioneSTEM.pdf /tmp/stem/
 mkdir -p /tmp/stem/docs/build/
 cp -prf docs/build/html/ /tmp/stem/docs/build/
